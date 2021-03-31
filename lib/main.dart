cc6ca24a02fba9aa6658b613d84af75ec420d930
osbornetunde <osbornetunde@gmail.com>
2021-03-31 22:13:26 +0100

    scaffolding a flutter app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello Tunde"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+      ),
+    ),
+  ));
+}
